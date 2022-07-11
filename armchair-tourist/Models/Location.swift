409874f4c281870a1ff98108305bb58ea71b52e0
Rick Brown <ricky.brown.00@gmail.com>
2022-07-11 11:32:27 +0100

    adds Location model and assets

---
+++ armchair-tourist/Models/Location.swift
@@ -0,0 +1,18 @@
+//
+//  Location.swift
+//  armchair-tourist
+//
+//  Created by Rick Brown on 11/07/2022.
+//
+
+import Foundation
+import MapKit
+
+struct Location {
+  let name: String
+  let cityName: String
+  let coordinates: CLLocationCoordinate2D
+  let description: String
+  let imageNames: Array<String>
+  let link: String
+}
