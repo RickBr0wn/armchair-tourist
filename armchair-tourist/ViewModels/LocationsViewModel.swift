2eb4febc696272663370d0125797c5f2aadb4093
Rick Brown <ricky.brown.00@gmail.com>
2022-07-11 11:59:22 +0100

    adds MapKit

--- armchair-tourist/ViewModels/LocationsViewModel.swift
+++ armchair-tourist/ViewModels/LocationsViewModel.swift
@@ -6,11 +6,34 @@
 //
 
 import Foundation
+import MapKit
+import SwiftUI
 
 class LocationsViewModel: ObservableObject {
   @Published var locations: Array<Location>
   
+  @Published var mapLocation: Location {
+    didSet {
+      updateMapRegion(location: mapLocation)
+    }
+  }
+  
+  @Published var mapRegion: MKCoordinateRegion = MKCoordinateRegion()
+  
+  let mapSpan: MKCoordinateSpan = MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
+  
   init() {
-    self.locations = LocationsDataService.locations
+    let locations = LocationsDataService.locations
+    let firstLocation = locations.first!
+    
+    self.locations = locations
+    self.mapLocation = firstLocation
+    self.updateMapRegion(location: firstLocation)
+  }
+  
+  private func updateMapRegion(location: Location) -> Void { 
+    withAnimation(.easeInOut) {
+      mapRegion = MKCoordinateRegion(center: location.coordinates, span: mapSpan)
+    }
   }
 }
