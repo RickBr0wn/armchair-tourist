d58e8eadb514a3c32a92759297d92552c2b32eb5
Rick Brown <ricky.brown.00@gmail.com>
2022-07-11 11:22:06 +0100

    Initial Commit

---
+++ armchair-tourist/armchair_touristApp.swift
@@ -0,0 +1,17 @@
+//
+//  armchair_touristApp.swift
+//  armchair-tourist
+//
+//  Created by Rick Brown on 11/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct armchair_touristApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
